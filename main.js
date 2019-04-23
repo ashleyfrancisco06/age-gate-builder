49e4441ba33c5e5b01f19aa631229be63b8fd53d
Ashley Francisco <Francisco.ashley1@gmail.com>
2019-04-23 00:57:03 -0400

    added formhandle function to prevent page refresh

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
-let DOB = document.getElementById("#dob").innerHTML
+let defaultVal = document.getElementById('dob').defaultValue = "1999-01";
+let DOB = document.getElementById("dob").value;
+console.log(defaultVal)
+console.log(DOB)
+let age = 2019 - DOB
+console.log(age)
+let form = document.querySelector('.form');
+console.log(form)
 
+function handleForm(e){
+	e.preventDefault();
+}
+
+form.addEventListener('submit', handleForm);
+
+function checkAge(){
+	console.log("clicked")
+	if( age >= 21){
+		alert ("Welcome");
+
+	}else{
+		alert ("Error.")
+	}
+}
 
 
 
