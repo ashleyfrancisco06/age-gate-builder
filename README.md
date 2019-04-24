5ba1548b710f8ddf008670e82e08f18c3fdb9b23
Ashley Francisco <Francisco.ashley1@gmail.com>
2019-04-23 20:15:42 -0400

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# age-gate-builder
-Creating an age gate that determines the users age and therefore determining accessibility to the site.
+# GOT Age Gate Builder
+Creating an age gate plugin that determines the users age and therefore determining accessibility to the site.
+
+https://res.cloudinary.com/ashleyfrancisco/image/upload/v1556064560/Screen_Shot_2019-04-23_at_8.08.05_PM.png
+
+## Deployed Site
+
+## Dependencies/Libraries/Frameworks Used
+
+ - HTML
+ - CSS
+ - JavaScript
+ - animate.css
+
+
+
+
+
+
--- main.js
+++ main.js
@@ -7,7 +7,6 @@ console.log(mm,yyyy)
 
 function monthOptions(){
 	const monthArr= ["January","February","March","April","May","June","July","August","September","October","November","December"];
-
 	//create each option
 	for(let i= 0; i<monthArr.length;i++){
 		//access select 
@@ -20,7 +19,6 @@ function monthOptions(){
 }
 
 function yearOptions(){
-
 	//create each option
 	for(let i=1970; i<=2019;i++){
 		//access select 
@@ -31,8 +29,6 @@ function yearOptions(){
 		choices.innerHTML=[i];
 	}
 }
-
-
 //prevent refresh after 
 let form = document.querySelector('.form');
 function handleForm(e){
@@ -45,7 +41,6 @@ function validateForm() {
   let nameVal = document.forms["form"]["name"].value;
   let monVal = document.forms["form"]["selMonth"].value;
   let yearVal = document.forms["form"]["selYear"].value;
-  // console.log(yearVal)
 
   if (nameVal == "" || monVal == "MM" || yearVal == "YYYY") {
     alert("You are missing one or more forms");
@@ -64,37 +59,24 @@ function checkAge(){
 	let validation=  document.getElementById('validation')
 
 	if(age >= 21){
-		
 		return(validation.innerHTML = "Welcome!")
-		
 	}else{
 		return(validation.innerHTML="Sorry,you're not old enough.")
 	}
 }
 
-//Remember User
-//put onclick for checkbox
-//when checkbox is checked, grab user input in username field
-//the push into usersArr
-//if user is in array, display "Welcome back" , otherwise checkA
 function saveUser(){
-	//user array
 	let usersArr= []
 	console.log(usersArr)
-	//select checkbox
 	let checkbox = document.getElementById("checkbox");
-	// checkbox.checked = false;
-	//select username field
 	let user = document.getElementById('name').value
-	console.log(user)
-	// if checkbox is checked, select user input field and grab value 
 	if(checkbox.checked == true){
 		return usersArr.push(user)
 		console.log(usersArr)
 	}
 	console.log(checkbox.checked)
-	//check array if there
 }
+
 function check() {
     document.getElementById("checkbox").checked = true;
 }
@@ -102,10 +84,5 @@ function check() {
 function uncheck() {
     document.getElementById("checkbox").checked = false;
 }
-
-
-
-
-
 
 
